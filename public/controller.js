aab6dd313123ec8b016db324b25c5d13885114f0
Ben <benfwatkins@gmail.com>
2022-04-12 21:40:53 +0400

    tracking emited motion

--- public/controller.js
+++ public/controller.js
@@ -1,33 +1,13 @@
 const socket = io();
 
-let motion = false;
-let ios = false;
-
 function getAccel(){
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission()
-      .then(function() {
-        console.log('DeviceMotionEvent enabled');
-
-        motion = true;
-        ios = true;
-      })
-      .catch(function(error) {
-        console.warn('DeviceMotionEvent not enabled', error);
-      })
-  } else {
-    // we are not on ios13 and above
-    // todo
-    // add detection for hardware for other devices
-    // if(got the hardware) {
-    // motion = true;
-    // }
-  }
-}
-
-if (motion) {
-  window.addEventListener('devicemotion', (event) => {
-    if (event.acceleration) {
-      socket.emit('motion', event.acceleration.y);
-  });
+    DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
+        window.addEventListener('devicemotion', (event) => {
+          if (event.acceleration) {
+            socket.emit('motion', event.acceleration.y);
+          }
+        });
+      }
+    });
 }
